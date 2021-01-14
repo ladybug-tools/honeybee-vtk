279b934edd2b013979ee9064882a03eae3ed772a
Devang <devngc@gmail.com>
2021-01-13 22:52:15 -0500

    build(*): Update support files

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: GNU Affero General Public License v3L",
         "Operating System :: OS Independent"
     ],
     license="AGPL-3.0"
