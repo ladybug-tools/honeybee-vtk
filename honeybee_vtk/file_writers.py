5fba7322cd715f5563f685afbd5e08f8e9513c6e
Devang <devngc@gmail.com>
2021-01-20 14:22:31 -0500

    refactor(file_writers): Create a separate folder before writing html

--- honeybee_vtk/file_writers.py
+++ honeybee_vtk/file_writers.py
@@ -4,7 +4,6 @@ import warnings
 import shutil
 import webbrowser
 
-from pathlib import Path
 from zipfile import ZipFile
 from honeybee.typing import clean_string
 from .hbjson import check_grid, read_hbjson, group_by_face_type
@@ -36,7 +35,6 @@ def write_files(file_path, file_name, target_folder, include_grids,
     Returns:
         A text string containing the path to the .zip file with VTK/VTP files.
     """
-
     # Get points and face_types from HBJSON
     points, hb_types = read_hbjson(hbjson)
 
@@ -100,7 +98,34 @@ def write_files(file_path, file_name, target_folder, include_grids,
 
 def write_html(file_path, file_name, target_folder, include_grids, include_vectors,
                hbjson):
+    """Write an HTML file with VTK objects embedded into it.
 
+    Args:
+        file_path: A text string for a valid path to the HBJSON file.
+        file_name: A text string for the name of the .zip file to be written. If no
+            text string is provided, the name of the HBJSON file will be used as a
+            file name for the .zip file.
+        target_folder: A text string to a folder to write the output file. The file
+            will be written to the current folder if not provided.
+        include_grids: A boolean. Defaults to True. Grids will not be extracted from
+            HBJSON if set to False.
+        include_vectors: A boolean. Defaults to True. Vector arrows will not be created
+            if set to False.
+        vtk_writer: A VTK writer object.
+        vtk_extension: A text string for the file extension to be used.
+        hbjson: A dictionary.
+
+    Returns:
+        A text string containing the path to the HTML file with VTK objects embedded.
+    """
+
+    # Create a new folder at target folder to store all the files
+    folder_name = 'hbjson-html'
+    target_folder = os.path.join(target_folder, folder_name)
+    if os.path.exists(target_folder):
+        shutil.rmtree(target_folder)
+
+    # Setting DataSet Writer and file extension
     vtk_writer = vtk.vtkJSONDataSetWriter()
     vtk_extension = ""
     
@@ -144,13 +169,13 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
 
     # remove extension if provided by user
     file_name = file_name if not file_name.lower().endswith('.zip') else file_name[:-4]
-    zip_name = file_name
+
     # Create a .zip file to capture all the generated .vtk files
-    # os.chdir(target_folder)
+    zip_name = file_name
     file_path = os.path.join(target_folder, file_name)
     shutil.make_archive(file_path, 'zip', target_folder)
  
-    # Delete vtk files if there is permission
+    # Delete the index.json if there is permission
     try:
         file_name = os.path.join(target_folder, 'index.json')
         os.remove(file_name)
@@ -159,7 +184,7 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
             'Honeybee does not have permission to delete index.json.'
             ' You may please delete the .vtk files manually.'
         )
-
+    # Delete the folders containing json datasets if there is permission
     for file_name in file_names:
         try:
             file_name = os.path.join(target_folder, file_name)
@@ -170,17 +195,23 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
                 ' You may please delete the .vtk files manually.'
             )
     
+    # Setting file names for three different formats
     zip_file = os.path.join(target_folder, zip_name + '.zip')
     vtkjs_file = os.path.join(target_folder, zip_name + '.vtkjs')
     html_file = os.path.join(target_folder, zip_name + '.html')
+
+    # Renaming .zip to .vtkjs
     os.rename(zip_file, vtkjs_file)
 
+    # Path to the Paraview Glance template
     html_path = '../honeybee-vtk/Paraview Glance/ParaViewGlance.html'
 
+    # Embed json datasets in HTML
     if os.path.exists(html_path):
         convertDirectoryToZipFile(vtkjs_file)
         addDataToViewer(vtkjs_file, html_path)
     
+    # Delete vtkjs file
     try:
         os.remove(vtkjs_file)
     except OSError:
@@ -190,5 +221,7 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
         )
     
     webbrowser.open(html_file)
-    
 
+    # File path to the HTML file
+    return html_file
+    
