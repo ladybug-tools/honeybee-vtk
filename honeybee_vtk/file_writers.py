fd38305f3ddcf4460b48b705c3f98defea3f4f65
Devang <devngc@gmail.com>
2021-01-21 16:16:35 -0500

    refactor(file_writers): Create files in a temp folder

--- honeybee_vtk/file_writers.py
+++ honeybee_vtk/file_writers.py
@@ -1,9 +1,10 @@
-"""Functions to write vtk/vtp or HTML files using vtk."""
+"""Functions to write vtk, vtp or HTML files using vtk."""
 
 import os
 import vtk
 import warnings
 import shutil
+import tempfile
 import webbrowser
 
 from zipfile import ZipFile
@@ -14,12 +15,13 @@ from .index import write_index_json
 from .vtkjs_helper import convertDirectoryToZipFile, addDataToViewer
 
 
-def write_files(file_path, file_name, target_folder, include_grids,
-                include_vectors, vtk_writer, vtk_extension, hbjson):
+def write_files(hbjson, file_path, file_name, target_folder, include_grids,
+                include_vectors, vtk_writer, vtk_extension):
     """
     Write a .zip of VTK/VTP files.
 
     Args:
+        hbjson: A dictionary.
         file_path: A text string for a valid path to the HBJSON file.
         file_name: A text string for the name of the .zip file to be written. If no
             text string is provided, the name of the HBJSON file will be used as a
@@ -30,13 +32,20 @@ def write_files(file_path, file_name, target_folder, include_grids,
             HBJSON if set to False.
         include_vectors: A boolean. Defaults to True. Vector arrows will not be created
             if set to False.
-        vtk_writer: A VTK writer object.
-        vtk_extension: A text string for the file extension to be used.
-        hbjson: A dictionary.
+        vtk_writer: A vtk object. Acceptable values are following;
+            vtk.vtkXMLPolyDataWriter() and vtk.vtkPolyDataWriter() to write XML and 
+            VTK files respectively.
+        vtk_extension: A text string for the file extension to be used. Following are 
+            acceptable values for the corresponding vtk_writer values;
+            vtk.vtkXMLPolyDataWriter() -> '.vtp',
+            vtk.vtkPolyDataWriter() -> '.vtk',  
 
     Returns:
         A text string containing the path to the .zip file with VTK/VTP files.
     """
+    # Create a temp folder
+    temp_folder = tempfile.mkdtemp()
+
     # Get points and face_types from HBJSON
     points, hb_types = read_hbjson(hbjson)
 
@@ -46,7 +55,7 @@ def write_files(file_path, file_name, target_folder, include_grids,
     # Write VTK files based on Honeybee face_type and Honeybee object type
     for hb_type in grouped_points:
         write_polydata(grouped_points[hb_type], hb_type, vtk_writer, vtk_extension,
-                       target_folder)
+                       temp_folder)
 
     # Names of VTK files to be written
     file_names = list(grouped_points.keys())
@@ -56,14 +65,14 @@ def write_files(file_path, file_name, target_folder, include_grids,
 
     # If grids are there in HBJSON and they are requested
     if grids and include_grids:
-        grid_file_names = _write_grids(grids, vtk_writer, vtk_extension, target_folder)
+        grid_file_names = _write_grids(grids, vtk_writer, vtk_extension, temp_folder)
         file_names.extend(grid_file_names)
 
     # Write vectors if they are requested
     if include_vectors:
-        to_write_vectors = [hb_types, grouped_points, grids, include_grids, vtk_writer,
-                            vtk_extension, target_folder]
-        vector_file_names = _write_vectors(*to_write_vectors)
+        vector_file_names = _write_vectors(
+            hb_types, grouped_points, grids, include_grids, vtk_writer, vtk_extension,
+            temp_folder)
         file_names.extend(vector_file_names)
 
     # Use the name of HBJSON if file name is not provided
@@ -74,35 +83,36 @@ def write_files(file_path, file_name, target_folder, include_grids,
     # remove extension if provided by user
     file_name = file_name if not file_name.lower().endswith('.zip') else file_name[:-4]
     
-    # Create a .zip file to capture all the generated .vtk files
-    zip_file = os.path.join(target_folder, file_name + '.zip')
-    zipobj = ZipFile(zip_file, 'w')
+    # Set a file path for the .zip file in the temp folder
+    temp_zip_file = os.path.join(temp_folder, file_name + '.zip')
+    # Set a file path to move the .zip file to the target folder
+    target_zip_file = os.path.join(target_folder, file_name + '.zip')
 
-    # Capture vtk files in a zip file.
-    os.chdir(target_folder)
-    for file_name in file_names:
-        zipobj.write(file_name + vtk_extension)
+    # # Capture vtk files in a zip file.
+    zipobj = ZipFile(temp_zip_file, 'w')
+    os.chdir(temp_folder)
+    for name in file_names:
+        name = os.path.join(name + vtk_extension)
+        zipobj.write(name)
     zipobj.close()
+    os.chdir(target_folder)
 
-    # Delete vtk files if there is permission
-    for file_name in file_names:
-        try:
-            file_name = os.path.join(target_folder, file_name + vtk_extension)
-            os.remove(file_name)
-        except OSError:
-            warnings.warn(
-                f'Honeybee does not have permission to delete {file_names}.'
-                ' You may please delete the .vtk files manually.'
-            )
+    # Move the generated HTML to target folder
+    shutil.move(temp_zip_file, target_zip_file)
+
+    # Remove temp folder
+    shutil.rmtree(temp_folder)
+
     # Return the path where the .zip file is written
-    return zip_file
+    return target_zip_file
 
 
-def write_html(file_path, file_name, target_folder, include_grids, include_vectors,
-               hbjson):
+def write_html(hbjson, file_path, file_name, target_folder, include_grids,
+               include_vectors, open_html):
     """Write an HTML file with VTK objects embedded into it.
 
     Args:
+        hbjson: A dictionary.
         file_path: A text string for a valid path to the HBJSON file.
         file_name: A text string for the name of the .zip file to be written. If no
             text string is provided, the name of the HBJSON file will be used as a
@@ -113,24 +123,26 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
             HBJSON if set to False.
         include_vectors: A boolean. Defaults to True. Vector arrows will not be created
             if set to False.
-        vtk_writer: A VTK writer object.
-        vtk_extension: A text string for the file extension to be used.
-        hbjson: A dictionary.
-
+        open_html: A boolean. If set to False, it will not open the generated HTML
+            in a web browser.
+        
     Returns:
         A text string containing the path to the HTML file with VTK objects embedded.
     """
+    # Path to the Paraview Glance template
+    html_path = '../honeybee-vtk/Paraview Glance/ParaViewGlance.html'
 
-    # Create a new folder at target folder to store all the files
-    folder_name = 'hbjson-html'
-    target_folder = os.path.join(target_folder, folder_name)
-    if os.path.exists(target_folder):
-        shutil.rmtree(target_folder)
+    # Make sure the template if found
+    if not os.path.exists(html_path):
+        raise FileNotFoundError('Paraview Glance HTML template not found.')
+
+    # Create a temp folder
+    temp_folder = tempfile.mkdtemp()
 
     # Setting DataSet Writer and file extension
     vtk_writer = vtk.vtkJSONDataSetWriter()
     vtk_extension = ""
-    
+
     # Get points and face_types from HBJSON
     points, hb_types = read_hbjson(hbjson)
 
@@ -140,7 +152,7 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
     # Write VTK files based on Honeybee face_type and Honeybee object type
     for hb_type in grouped_points:
         write_polydata(grouped_points[hb_type], hb_type, vtk_writer, vtk_extension,
-                       target_folder)
+                       temp_folder)
 
     # Names of VTK files to be written
     file_names = list(grouped_points.keys())
@@ -150,20 +162,20 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
 
     # If grids are there in HBJSON and they are requested
     if grids and include_grids:
-        grid_file_names = _write_grids(grids, vtk_writer, vtk_extension, target_folder)
+        grid_file_names = _write_grids(grids, vtk_writer, vtk_extension, temp_folder)
         file_names.extend(grid_file_names)
 
     # Write vectors if they are requested
     if include_vectors:
-        to_write_vectors = [hb_types, grouped_points, grids, include_grids, vtk_writer,
-                            vtk_extension, target_folder]
-        vector_file_names = _write_vectors(*to_write_vectors)
+        vector_file_names = _write_vectors(
+            hb_types, grouped_points, grids, include_grids, vtk_writer, vtk_extension,
+            temp_folder
+        )
         file_names.extend(vector_file_names)
 
     # Write index.json
-    write_index_json(target_folder, file_names)
+    write_index_json(temp_folder, file_names)
 
-    # Collect everything in a .zip
     # Use the name of HBJSON if file name is not provided
     if not file_name:
         name = '.'.join(os.path.basename(file_path).split('.')[:-1])
@@ -172,58 +184,32 @@ def write_html(file_path, file_name, target_folder, include_grids, include_vecto
     # remove extension if provided by user
     file_name = file_name if not file_name.lower().endswith('.zip') else file_name[:-4]
 
-    # Create a .zip file to capture all the generated .vtk files
-    zip_name = file_name
-    file_path = os.path.join(target_folder, file_name)
-    shutil.make_archive(file_path, 'zip', target_folder)
+    # Create a .zip file to capture all the generated json dataset folders
+    file_path = os.path.join(temp_folder, file_name)
+    shutil.make_archive(file_path, 'zip', temp_folder)
  
-    # Delete the index.json if there is permission
-    try:
-        file_name = os.path.join(target_folder, 'index.json')
-        os.remove(file_name)
-    except OSError:
-        warnings.warn(
-            'Honeybee does not have permission to delete index.json.'
-            ' You may please delete the .vtk files manually.'
-        )
-    # Delete the folders containing json datasets if there is permission
-    for file_name in file_names:
-        try:
-            file_name = os.path.join(target_folder, file_name)
-            shutil.rmtree(file_name)
-        except OSError:
-            warnings.warn(
-                f'Honeybee does not have permission to delete {file_names}.'
-                ' You may please delete the .vtk files manually.'
-            )
-    
-    # Setting file names for three different formats
-    zip_file = os.path.join(target_folder, zip_name + '.zip')
-    vtkjs_file = os.path.join(target_folder, zip_name + '.vtkjs')
-    html_file = os.path.join(target_folder, zip_name + '.html')
+    # Setting file paths
+    zip_file = os.path.join(temp_folder, file_name + '.zip')
+    vtkjs_file = os.path.join(temp_folder, file_name + '.vtkjs')
+    temp_html_file = os.path.join(temp_folder, file_name + '.html')
+    target_html_file = os.path.join(target_folder, file_name + '.html')
 
     # Renaming .zip to .vtkjs
     os.rename(zip_file, vtkjs_file)
 
-    # Path to the Paraview Glance template
-    html_path = '../honeybee-vtk/Paraview Glance/ParaViewGlance.html'
-
     # Embed json datasets in HTML
-    if os.path.exists(html_path):
-        convertDirectoryToZipFile(vtkjs_file)
-        addDataToViewer(vtkjs_file, html_path)
-    
-    # Delete vtkjs file
-    try:
-        os.remove(vtkjs_file)
-    except OSError:
-        warnings.warn(
-            'Honeybee does not have permission to delete index.json.'
-            ' You may please delete the .vtk files manually.'
-        )
-    
-    webbrowser.open(html_file)
+    convertDirectoryToZipFile(vtkjs_file)
+    addDataToViewer(vtkjs_file, html_path)
 
-    # File path to the HTML file
-    return html_file
+    # Move the generated HTML to target folder
+    shutil.move(temp_html_file, target_html_file)
+
+    # Remove temp folder
+    shutil.rmtree(temp_folder)
+
+    # Open the generated HTML in a browser if requested
+    if open_html:
+        webbrowser.open(target_html_file)
+
+    return target_html_file
     
