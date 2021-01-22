2e75ac5837cd31789c3b4cb2f9007b0d606b4750
Devang <devngc@gmail.com>
2021-01-22 18:24:58 -0500

    test(write_test): Update test

--- tests/write_test.py
+++ tests/write_test.py
@@ -10,13 +10,13 @@ def test_write():
     zip_path = writer(file_path, writer='vtk', target_folder='./tests/assets/temp')
     assert os.path.isfile(zip_path)
     zip_file = zipfile.ZipFile(zip_path)
-    assert len(zip_file.namelist()) == 11
+    assert len(zip_file.namelist()) == 6
 
 
 def test_write_no_grids():
     file_path = './tests/assets/unnamed.hbjson'
     # Write without grids
-    zip_path = writer(file_path, include_grids=False, writer='vtk',
+    zip_path = writer(file_path, include_grids=False, include_vectors=True, writer='vtk',
                       target_folder='./tests/assets/temp')
     assert os.path.isfile(zip_path)
     zip_file = zipfile.ZipFile(zip_path)
@@ -26,7 +26,7 @@ def test_write_no_grids():
 def test_write_no_vectors():
     file_path = './tests/assets/unnamed.hbjson'
     # Write without vectors
-    zip_path = writer(file_path, include_vectors=False, writer='vtk',
+    zip_path = writer(file_path, include_grids=True, include_vectors=False, writer='vtk',
                       target_folder='./tests/assets/temp')
     assert os.path.isfile(zip_path)
     zip_file = zipfile.ZipFile(zip_path)
