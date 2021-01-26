c1419de2b0a6e046a6374523b1fbf687013ae3be
Devang <devngc@gmail.com>
2021-01-25 20:38:45 -0500

    refactor(cli): Update cli options

--- honeybee_vtk/cli.py
+++ honeybee_vtk/cli.py
@@ -31,29 +31,31 @@ def main():
     show_default=True
 )
 @click.option(
-    '--include-grids', '-eg', is_flag=True, default=False,
+    '--include-grids', '-ig', is_flag=True, default=False,
     help='Export grids.', show_default=True
 )
 @click.option(
-    '--include-vectors', '-ev', is_flag=True, default=False,
-    help='Export normals for apertures and grid sensors.', show_default=True
+    '--include-sensors', '-is', type=click.Choice(['vectors', 'points', '']),
+    default='',
+    help='Export sensors as either arrows or color-grouped points.', show_default=True
 )
 @click.option(
-    '--include-points', '-ep', is_flag=True, default=False,
-    help='Export points colored based on the direction of normal for apertures' 
-    ' and grid sensors.', show_default=True
+    '--include-normals', '-in', type=click.Choice(['vectors', 'points', '']),
+    default='',
+    help='Export aperture normals as either arrows or color-grouped points.',
+    show_default=True
 )
 @click.option(
     '--show-html', '--show', '-sh', is_flag=True, default=False,
-    help='Open the generated HTML file in a browser', show_default=True
+    help='Open the generated HTML file in a browser.', show_default=True
 )
 def translate_recipe(
-        hbjson_file, name, folder, file_type, include_grids, include_vectors,
-        include_points, show_html
+        hbjson_file, name, folder, file_type, include_grids, include_sensors,
+        include_normals, show_html
     ):
     """Translate a HBJSON file to several VTK, XML, or HTML file.
 
-    The output file is either a zipped file that contains all the generated VTK/XML files 
+    The output file is either a zipped file that contains all the generated VTK/XML files
     or an HTML file.
 
     \b
@@ -63,13 +65,17 @@ def translate_recipe(
     """
 
     folder = pathlib.Path(folder)
-    folder.mkdir(exist_ok=True)
+    folder.mkdir(exist_ok = True)
+    if include_sensors == '':
+        include_sensors = False
+    if include_normals == '':
+        include_normals = False
 
     try:
         output_file = writer(
             hbjson_file, target_folder=folder, file_name=name,
-            include_grids=include_grids, include_vectors=include_vectors,
-            include_points=include_points, writer=file_type, open_html=show_html
+            include_grids=include_grids, include_sensors=include_sensors,
+            include_normals=include_normals, writer=file_type, open_html=show_html
         )
     except Exception as e:
         raise ClickException(f'Translation to VTK failed:\n{e}')
