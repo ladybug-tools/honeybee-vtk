7a0d3e2732f6f76c5e9df76b976bce00f1516d9c
Devang <devngc@gmail.com>
2021-04-20 20:36:35 +0530

    feat(cli): update cli

--- honeybee_vtk/cli.py
+++ honeybee_vtk/cli.py
@@ -6,6 +6,8 @@ import pathlib
 import click
 from click.exceptions import ClickException
 
+from honeybee_vtk.model import Model
+from honeybee_vtk.vtkjs.schema import SensorGridOptions, DisplayMode
 
 @click.group()
 def main():
@@ -16,8 +18,7 @@ def main():
 @main.command('translate')
 @click.argument('hbjson-file')
 @click.option(
-    '--name', '-n', help='Name of the output file. If not provided, the name of '
-    'input HBJSON file will be used.'
+    '--name', '-n', help='Name of the output file.', default="model", show_default=True
 )
 @click.option(
     '--folder', '-f', help='Path to target folder.',
@@ -25,57 +26,65 @@ def main():
     default='.', show_default=True
 )
 @click.option(
-    '--file-type', '-t', type=click.Choice(['vtk', 'xml', 'html']), default='html',
-    help='Switch between VTK, XML, and HTML formats. Default is HTML.',
-    show_default=True
+    '--file-type', '-ft', type=click.Choice(['html', 'vtkjs']), default='html',
+    help='Switch between html and vtkjs formats', show_default=True
 )
 @click.option(
-    '--include-grids', '-ig', is_flag=True, default=False,
-    help='Export grids.', show_default=True
+    '--display-mode', '-dm', type=click.Choice(['shaded', 'surface',
+        'surfacewithedges', 'wireframe', 'points']), help='Set display mode for the'
+        ' model.'
 )
 @click.option(
-    '--include-sensors', '-is', type=click.Choice(['vectors', 'points']),
-    help='Export sensors as either arrows or color-grouped points.', show_default=True
-)
-@click.option(
-    '--include-normals', '-in', type=click.Choice(['vectors', 'points']),
-    help='Export aperture normals as either arrows or color-grouped points.',
-    show_default=True
+    '--grid-options', '-go', type=click.Choice(['ignore', 'points', 'meshes']),
+    help='Export sensor grids as either points or meshes.'
 )
 @click.option(
     '--show-html', '--show', '-sh', is_flag=True, default=False,
     help='Open the generated HTML file in a browser.', show_default=True
 )
 def translate_recipe(
-        hbjson_file, name, folder, file_type, include_grids, include_sensors,
-        include_normals, show_html
-    ):
-    """Translate a HBJSON file to several VTK, XML, or HTML file.
-
-    The output file is either a zipped file that contains all the generated VTK/XML files
-    or an HTML file.
+        hbjson_file, name, folder, file_type, display_mode, grid_options, show_html):
+    """Translate a HBJSON file to an HTML or a vtkjs file.
 
     \b
     Args:
-        hbjson-file: Path to input HBJSON file.
+        hbjson-file: Path to an HBJSON file.
 
     """
-
     folder = pathlib.Path(folder)
     folder.mkdir(exist_ok=True)
-    if not include_normals:
-        include_normals = False
-    if not include_sensors:
-        include_sensors = False
+
+    # Set Sensor grids
+    if grid_options == 'ignore':
+        grid_options = SensorGridOptions.Ignore
+    elif grid_options == 'points':
+        grid_options = SensorGridOptions.Sensors
+    elif grid_options == 'meshes':
+        grid_options = SensorGridOptions.Mesh
 
     try:
-        output_file = writer(
-            hbjson_file, target_folder=folder, file_name=name,
-            include_grids=include_grids, include_sensors=include_sensors,
-            include_normals=include_normals, writer=file_type, open_html=show_html
-        )
+        model = Model.from_hbjson(hbjson=hbjson_file, load_grids=grid_options)
+
+        # Set display style
+        if display_mode == 'shaded':
+            model.update_display_mode(DisplayMode.Shaded)
+        elif display_mode == 'surface':
+            model.update_display_mode(DisplayMode.Surface)
+        elif display_mode == 'surfacewithedges':
+            model.update_display_mode(DisplayMode.SurfaceWithEdges)
+        elif display_mode == 'wireframe':
+            model.update_display_mode(DisplayMode.Wireframe)
+        elif display_mode == 'points':
+            model.update_display_mode(DisplayMode.Points)
+
+        # Set file type
+        if file_type.lower() == 'html':
+            output = model.to_html(folder=folder, name=name, show=show_html)
+        else:
+            output = model.to_vtkjs(folder=folder, name=name)
+
     except Exception as e:
-        raise ClickException(f'Translation to VTK failed:\n{e}')
+        raise ClickException(f'Translation failed:\n{e}')
     else:
-        print(f'Success: {output_file}', file=sys.stderr)
+        print(f'Success: {output}', file=sys.stderr)
         return sys.exit(0)
