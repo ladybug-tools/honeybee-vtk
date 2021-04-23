3198b139a700ee964c34724d5937629b98e1c345
Devang <devngc@gmail.com>
2021-04-23 09:27:48 +0530

    fix(cli): make optional argument not case sensitive

--- honeybee_vtk/cli.py
+++ honeybee_vtk/cli.py
@@ -22,21 +22,22 @@ def main():
 )
 @click.option(
     '--folder', '-f', help='Path to target folder.',
-    type=click.Path(exists=False, file_okay=False, resolve_path=True, dir_okay='True'),
-    default='.', show_default=True
+    type=click.Path(exists=False, file_okay=False, resolve_path=True,
+    dir_okay='True'), default='.', show_default=True
 )
 @click.option(
-    '--file-type', '-ft', type=click.Choice(['html', 'vtkjs']), default='html',
-    help='Switch between html and vtkjs formats', show_default=True
+    '--file-type', '-ft', type=click.Choice(['html', 'vtkjs'], case_sensitive=False),
+    default='html', help='Switch between html and vtkjs formats', show_default=True
 )
 @click.option(
     '--display-mode', '-dm', type=click.Choice(['shaded', 'surface',
-        'surfacewithedges', 'wireframe', 'points']), help='Set display mode for the'
-        ' model.'
+    'surfacewithedges', 'wireframe', 'points'], case_sensitive=False),
+    default='shaded', help='Set display mode for the model.', show_default=True
 )
 @click.option(
-    '--grid-options', '-go', type=click.Choice(['ignore', 'points', 'meshes']),
-    help='Export sensor grids as either points or meshes.'
+    '--grid-options', '-go', type=click.Choice(['ignore', 'points', 'meshes'],
+    case_sensitive=False), default='ignore', help='Export sensor grids as either'
+    ' points or meshes.', show_default=True,
 )
 @click.option(
     '--show-html', '--show', '-sh', is_flag=True, default=False,
@@ -78,11 +79,11 @@ def translate_recipe(
             model.update_display_mode(DisplayMode.Points)
 
         # Set file type
-        if file_type.lower() == 'html':
+        if file_type == 'html':
             output = model.to_html(folder=folder, name=name, show=show_html)
         else:
             output = model.to_vtkjs(folder=folder, name=name)
-
+        print(output)
     except Exception as e:
         raise ClickException(f'Translation failed:\n{e}')
     else:
