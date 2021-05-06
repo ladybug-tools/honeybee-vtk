ab62e860c904369927e5e31d23ea88b5c74ecfb5
Devang <devngc@gmail.com>
2021-05-06 23:39:27 +0530

    feat(scene): add capability to export multiple images

--- honeybee_vtk/scene.py
+++ honeybee_vtk/scene.py
@@ -1,4 +1,5 @@
-"""A VTK rendering scene."""
+"""A VTK scene."""
+
 import pathlib
 import enum
 from typing import Tuple
@@ -22,40 +23,76 @@ class ImageTypes(enum.Enum):
 
 
 class Scene(object):
-    """A rendering scene with a single viewport.
+    """Initialize a Scene object.
 
-    Once as scene is created you can export it to glTF, an image or just show it in an
-    interactive viewer.
+    Vtk derives inspiration from a movie set in naming its objects. Imagine a scene
+    being prepared at a movie set. The scene has a background. It has a few actors.
+    And, there are a few cameras setup to capture the scene from different angles. A
+    scene is vtk is defined in the similar fashion. It has a background color, some
+    actors i.e. geometry objects from model, and a few cameras around the scene.
 
+    Args:
+        background_color: A tuple of three integers that represent RGB values of the
+            color that you'd like to set as the background color. Defaults to gray.
+        model: A Model camera object created using honeybee-vtk.
+        cameras: A list of vtk camera objects. Defaults to one camera that shows the
+            scene from top.
+        monochrome: A boolean value. If set to True, one color will be applied to all
+            the geometry objects in Scene. This is especially useful when
+            the DisplayMode is set to Wireframe and results are going to be
+            loaded to the model.
+        monochrome_color: A tuple of decimal numbers to represent RGB color.
+            Defaults to gray.
     """
-    __slots__ = ('_cameras', '_renderer', '_window', '_interactor',
-                 '_monochrome', '_monochrome_color')
+    __slots__ = ('_background_color', '_model', '_cameras', '_monochrome',
+                 '_monochrome_color')
 
-    def __init__(self, background_color=None, cameras=None, monochrome=None,
+    def __init__(self, background_color=None, model=None, cameras=None, monochrome=None,
                  monochrome_color=None) -> None:
-        """Initialize a Scene object.
 
-        Args:
-            background_color: A tuple of three floats that represent RGB values of the
-                color that you'd like to set as the background color. Defaults to None.
-            cameras: A list of vtk camera objects. Defaults to None.
-            monochrome: A boolean value. If set to True, one color will be applied to all
-                the geometry objects in Scene. This is especially useful when
-                the DisplayMode is set to Wireframe and results are going to be
-                loaded to the model.
-            monochrome_color: A tuple of decimal numbers to represent RGB color.
-                Defaults to gray color.
-        """
+        self.background_color = background_color
+        self.model = model
         self.cameras = cameras
-        interactor, window, renderer = self._create_render_window(background_color)
-        self._renderer = renderer
-        self._window = window
-        self._interactor = interactor
         self.monochrome = monochrome
         self.monochrome_color = monochrome_color
 
     @property
+    def background_color(self):
+        """background_color for the scene."""
+        return self._background_color
+
+    @background_color.setter
+    def background_color(self, val):
+        if not val:
+            colors = vtk.vtkNamedColors()
+            self._background_color = colors.GetColor3d("SlateGray")
+        elif _check_tuple(val, int):
+            self._background_color = val
+        else:
+            raise ValueError(
+                'Background color is a tuple with three integers'
+                ' representing R, G, and B values.'
+            )
+
+    @property
+    def model(self):
+        """Model object to be added to the scene which will turn into actors."""
+        return self._model
+
+    @model.setter
+    def model(self, val):
+        if isinstance(val, Model):
+            self._model = val
+        elif not val:
+            self._model = None
+        else:
+            raise ValueError(
+                f'A Model object created using honeybee-vtk required. Intead got {val}.'
+            )
+
+    @property
     def monochrome(self):
+        """Switch for monochrome colors."""
         return self._monochrome
 
     @monochrome.setter
@@ -71,6 +108,7 @@ class Scene(object):
 
     @property
     def monochrome_color(self):
+        """Color to be used in monochrome mode."""
         return self._monochrome_color
 
     @monochrome_color.setter
@@ -87,7 +125,7 @@ class Scene(object):
 
     @property
     def cameras(self):
-        """A list of vtk cameras."""
+        """A list of vtk cameras setup in the scene."""
         return self._cameras
 
     @cameras.setter
@@ -102,7 +140,7 @@ class Scene(object):
         else:
             raise ValueError(
                 'Either a list of vtk camera objects or a vtk camera object is expected.'
-                f' Instead got {val}.'
+                f' Instead got {val}. Make sure to use to_vtk method.'
             )
 
     def add_cameras(self, val):
@@ -118,35 +156,40 @@ class Scene(object):
         else:
             raise ValueError(
                 'Either a list of vtk camera objects or a vtk camera object is expected.'
-                f' Instead got {val}.'
+                f' Instead got {val}. Make sure to use to_vtk method.'
             )
 
-    def _create_render_window(self, background_color=None) \
-            -> Tuple[
-                vtk.vtkRenderWindowInteractor, vtk.vtkRenderWindow, vtk.vtkRenderer
-            ]:
+    def create_render_window(self, camera=None) \
+        -> Tuple[
+            vtk.vtkRenderWindowInteractor, vtk.vtkRenderWindow, vtk.vtkRenderer
+        ]:
         """Create a rendering window with a single renderer and an interactor.
 
-        The objects are embedded inside each other:
-        interactor is the outmost layer. A render is added to a window and then the
-        window is set inside the interactor.
-
-        If you are thinking why, the answer is that is how VTK is designed to work.
+        The objects are embedded inside each other. interactor is the outmost layer.
+        A render is added to a window and then the window is set inside the interactor.
 
         Args:
-            background_color: A tuple of three floats that represent RGB values of the
-                color that you'd like to set as the background color. Defaults to None.
-            cameras: Either a list of vtk camera objects or a vtk camera object.
+            camera: A vtk camera object.
 
         Returns:
-            Tuple -- window_interactor, render_window, renderer
+            A tuple of following;
+
+            -   window_interactor,
+            -   render_window,
+            -   renderer
         """
-        # Setting camera
-        camera = self.cameras[0]
+        # if camera is not provided use the first camera in the list of cameras setup
+        # in the scene
+        if not camera:
+            camera = self._cameras[0]
 
         # Setting renderer, render window, and interactor
         renderer = vtk.vtkRenderer()
         renderer.SetActiveCamera(camera)
+
+        # Add actors to the window if model(actor) has arrived at the scene
+        if self._model:
+            self.add_model(self._model, renderer)
 
         # add renderer to rendering window
         window = vtk.vtkRenderWindow()
@@ -156,32 +199,22 @@ class Scene(object):
         interactor = vtk.vtkRenderWindowInteractor()
         interactor.SetRenderWindow(window)
 
-        # Validate background color and set it for the render window
-        if not background_color:
-            colors = vtk.vtkNamedColors()
-            background_color = colors.GetColor3d("SlateGray")
-
-        elif isinstance(background_color, tuple) and len(background_color) == 3\
-                and _check_tuple(background_color, int):
-            pass
-        else:
-            raise ValueError(
-                'Background color is a tuple with three integers'
-                ' representing R, G, and B values.'
-            )
-
-        renderer.SetBackground(background_color)
+        # Set background color
+        renderer.SetBackground(self._background_color)
         renderer.TwoSidedLightingOn()
 
         # return the objects - the order is from outside to inside
         return interactor, window, renderer
 
-    def get_legend(self, color_range) -> vtk.vtkScalarBarWidget():
+    def get_legend(self, color_range, interactor) -> vtk.vtkScalarBarWidget():
         """Create a scalar bar widget.
 
         Args:
             color_range: A VTK LookUpTable object for color range. You can create one
                 from color_range method in `DataFieldInfo`.
+            interactor: A vtk renderwindowinteractor object that can be created using
+                the create_render_window method.
+
         Returns:
             A VTK scalar bar widget.
         """
@@ -192,19 +225,32 @@ class Scene(object):
 
         # create the scalar_bar_widget
         scalar_bar_widget = vtk.vtkScalarBarWidget()
-        scalar_bar_widget.SetInteractor(self._interactor)
+        scalar_bar_widget.SetInteractor(interactor)
         scalar_bar_widget.SetScalarBarActor(scalar_bar)
         return scalar_bar_widget
 
-    def add_model(self, model: Model):
-        """Add a model to scene."""
+    def add_model(self, model: Model, renderer):
+        """Add a model to the scene.
+
+        Args:
+            model: A Model camera object created using honeybee-vtk.
+            renderer: A vtk renderer object created using create_render_window method.
+        """
         for ds in model:
             if ds.is_empty:
                 continue
-            self.add_dataset(ds)
+            self._add_dataset(ds, renderer)
 
-    def add_dataset(self, data_set: ModelDataSet):
-        """Create a dataset to scene as a VTK actor."""
+    def _add_dataset(self, data_set: ModelDataSet, renderer):
+        """Add a dataset to scene as a VTK actor.
+
+        This method is only used in add_model method.
+
+        Args:
+            data_set: A ModelDataSet object from a Model object created using
+                honeybee-vtk.
+            renderer: A vtk renderer object created using create_render_window method.
+        """
 
         # calculate point data based on cell data
         cell_to_point = vtk.vtkCellDataToPointData()
@@ -244,25 +290,32 @@ class Scene(object):
         if data_set.display_mode == DisplayMode.Wireframe:
             actor.GetProperty().SetRepresentationToWireframe()
 
-        self._renderer.AddActor(actor)
+        renderer.AddActor(actor)
 
-    def to_gltf(self, folder, name):
-        """Save the scene to a glTF file.
+    def export_gltf(self, folder, renderer, window, name):
+        """Export a scene to a glTF file.
 
         Args:
             folder: A valid path to where you'd like to write the gltf file.
+            renderer: A vtk renderer object created using create_render_window method.
+            window: A vtk rendererwindow object created using
+                create_render_window method.
             name: Name of the gltf file as a text string.
 
         Returns:
             A text string representing the path to the gltf file.
         """
+        # TODO: Find out why model's displaymode is not applied
+        # TODO: Find out if the axis should be rotated to work with gltf viewers
+        # TODO: Find a viewer for gltf files. The up axis of f3d viewer is Y axis.
+
         gltf_file_path = pathlib.Path(folder, f'{name}.gltf')
         exporter = vtk.vtkGLTFExporter()
         exporter.SaveNormalOn()
         exporter.InlineDataOn()
         exporter.SetFileName(gltf_file_path.as_posix())
-        exporter.SetActiveRenderer(self._renderer)
-        exporter.SetRenderWindow(self._window)
+        exporter.SetActiveRenderer(renderer)
+        exporter.SetRenderWindow(window)
         exporter.Modified()
         exporter.Write()
         return gltf_file_path.as_posix()
@@ -286,23 +339,30 @@ class Scene(object):
             raise ValueError(f'Invalid image type: {image_type}')
         return writer
 
-    def to_image(
-        self, folder, name, image_type: ImageTypes = ImageTypes.png, *, rgba=False,
-        image_scale=1, width=2400, height=2000, color_range=None, show=False
-            ):
-        """Save scene to an image.
+    def export_image(self, folder, name, window, interactor,
+                     image_type: ImageTypes = ImageTypes.png,
+                     *, rgba=False, image_scale=1, image_width=2400, image_height=2000,
+                     color_range=None, show=False):
+        """Export the scene as an image.
+
         Reference: https://kitware.github.io/vtk-examples/site/Python/IO/ImageWriter/
+        This method is able to export an image in '.png', '.jpg', '.ps', '.tiff', '.bmp',
+        and '.pnm' formats.
 
         Args:
             folder: A valid path to where you'd like to write the image.
             name: Name of the image as a text string.
-            image_type: An ImageType object
+            window: A vtk rendererwindow object created using
+                create_render_window method.
+            interactor: A vtk renderwindowinteractor object created using
+                create_render_window method.
+            image_type: An ImageType object.
             rgba: A boolean value to set the type of buffer. A True value sets
                 an the background color to black. A False value uses the Scene object's
                 background color. Defaults to False.
             image_scale: An integer value as a scale factor. Defaults to 1.
-            width: An integer value that sets the width of image in pixels.
-            height: An integer value that sets the height of image in pixels.
+            image_width: An integer value that sets the width of image in pixels.
+            image_height: An integer value that sets the height of image in pixels.
             color_range: A vtk lookup table object which can be obtained
                 from the color_range mehtod of the DataFieldInfo object.
                 Defaults to None.
@@ -320,15 +380,20 @@ class Scene(object):
 
             # create the scalar_bar_widget
             legend = vtk.vtkScalarBarWidget()
-            legend.SetInteractor(self._interactor)
+            legend.SetInteractor(interactor)
             legend.SetScalarBarActor(scalar_bar)
             legend.On()
 
         # render window
         if not show:
-            self._window.OffScreenRenderingOn()
-        self._window.SetSize(width, height)
-        self._window.Render()
+            window.OffScreenRenderingOn()
+            window.SetSize(image_width, image_height)
+            window.Render()
+        else:
+            window.SetSize(width, height)
+            window.Render()
+            interactor.Initialize()
+            interactor.Start()
 
         image_path = pathlib.Path(folder, f'{name}.{image_type.value}')
         writer = self._get_image_writer(image_type)
@@ -336,7 +401,7 @@ class Scene(object):
             writer.SetQuality(100)  # image quality
 
         window_to_image_filter = vtk.vtkWindowToImageFilter()
-        window_to_image_filter.SetInput(self._window)
+        window_to_image_filter.SetInput(window)
         window_to_image_filter.SetScale(image_scale)
 
         # rgba is not supported for postscript image type
@@ -357,14 +422,41 @@ class Scene(object):
 
         return image_path.as_posix()
 
-    # TODO: Color range input is a hack - we should keep track of the data
-    # that is added to the scene and reuse them
-    def show(self, color_range=None):
-        """Show rendered view."""
-        if color_range:
-            legend = self.get_legend(color_range)
-            legend.On()
-        self._window.Render()
-        self._interactor.Start()
-        if color_range:
-            legend.Off()
+    def export_images(
+        self, folder, name, image_type: ImageTypes = ImageTypes.png, *, rgba=False,
+        image_scale=1, image_width=2400, image_height=2000, color_range=None
+            ):
+        """Export all the cameras setup in a scene as images.
+
+        This method calls export_image method under the hood.
+
+        Args:
+            folder: A valid path to where you'd like to write the images.
+            name: A text string that will be used as a name for the images.
+            image_type: An ImageType object.
+            rgba: A boolean value to set the type of buffer. A True value sets
+                an the background color to black. A False value uses the Scene object's
+                background color. Defaults to False.
+            image_scale: An integer value as a scale factor. Defaults to 1.
+            image_width: An integer value that sets the width of image in pixels.
+            image_height: An integer value that sets the height of image in pixels.
+            color_range: A vtk lookup table object which can be obtained
+                from the color_range mehtod of the DataFieldInfo object.
+                Defaults to None.
+
+        Returns:
+            A tuple of paths to each exported image.
+        """
+        file_paths = []
+
+        for count, camera in enumerate(self._cameras):
+            # Create a render window for each camera setup in the scene.
+            interactor, window = self.create_render_window(camera)[0:2]
+            file_paths.append(
+                self.export_image(folder=folder, name=name + '_' + str(count),
+                                  window=window, interactor=interactor,
+                                  image_type=image_type, rgba=rgba,
+                                  image_scale=image_scale, image_width=image_width,
+                                  image_height=image_height, color_range=color_range)
+                                  )
+        return tuple(file_paths)
