0dcbb5dc2b9d75b2842f7bab9ba31026b8377740
Devang <devngc@gmail.com>
2021-05-06 23:39:50 +0530

    test(scene_test): update unit tests and add tests

--- tests/scene_test.py
+++ tests/scene_test.py
@@ -30,28 +30,25 @@ def test_write_gltf():
     model.sensor_grids.add_data_fields(daylight_factor, name='Daylight-factor',
                                        per_face=True, data_range=(0, 20))
     model.sensor_grids.color_by = 'Daylight-factor'
-
     model.sensor_grids.display_mode = DisplayMode.SurfaceWithEdges
-    model.update_display_mode(DisplayMode.Wireframe)
+    model.update_display_mode(DisplayMode.Shaded)
 
-    scene = Scene(background_color=(0,0,0))
-    scene.add_model(model)
+    scene = Scene(background_color=(0,0,0), model=model)
+    interactor, window, renderer = scene.create_render_window()
 
     if os.path.isdir(target_folder):
         shutil.rmtree(target_folder)
-
     os.mkdir(target_folder)
 
-    scene.to_gltf(target_folder, name='daylight-factor')
-
+    scene.export_gltf(target_folder, renderer, window, name='daylight-factor')
     gltf_path = os.path.join(target_folder, 'daylight-factor.gltf')
     assert os.path.isfile(gltf_path)
-
     shutil.rmtree(target_folder)
 
 
 def test_image_types():
     """Tests all the image types."""
+
     assert ImageTypes.png.value == 'png'
     assert ImageTypes.jpg.value == 'jpg'
     assert ImageTypes.ps.value == 'ps'
@@ -62,32 +59,41 @@ def test_image_types():
 
 def test_class_initialization():
     """Test if the attributes of the class are set correctly."""
+
     scene = Scene(background_color=(255, 255, 255))
-    assert isinstance(scene._interactor, vtk.vtkRenderWindowInteractor)
-    assert isinstance(scene._window, vtk.vtkRenderWindow)
-    assert isinstance(scene._renderer, vtk.vtkRenderer)
     assert scene.monochrome is False
+
+    # Check default monochrome color
     scene = Scene(background_color=(255, 255, 255), monochrome=True)
     assert scene.monochrome_color == (0.54, 0.54, 0.54)
+
+    # Check that decimal values not allowed in background color
     with pytest.raises(ValueError):
         scene = Scene(background_color=(123.24, 23, 255))
+
+    # Check that integer values not allowed in monochrome color
     with pytest.raises(ValueError):
         scene = Scene(background_color=(255, 255, 255), monochrome=True,
                       monochrome_color=(0,0,0))
+
+    # Check a vtk camera object is attached by default
     assert isinstance(scene.cameras[0], vtk.vtkCamera)
 
 
 def test_legend():
     """Test legend."""
+
     data_field = DataFieldInfo()
     color_range = data_field.color_range()
     scene = Scene()
-    legend = scene.get_legend(color_range)
+    interactor = scene.create_render_window()[0]
+    legend = scene.get_legend(color_range, interactor)
     assert isinstance(legend, vtk.vtkScalarBarWidget)
 
 
 def test_actors_in_scene():
     """Test if all the dataset in a model are being added to the scene."""
+
     file_path = r'./tests/assets/gridbased.hbjson'
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
 
@@ -99,13 +105,14 @@ def test_actors_in_scene():
     assert non_empty_datasets == 6
 
     # Test that all the non-empty datasets are being added to the scene
-    scene = Scene()
-    scene.add_model(model)
-    assert scene._renderer.VisibleActorCount() == 6
+    scene = Scene(model=model)
+    renderer = scene.create_render_window()[2]
+    assert renderer.VisibleActorCount() == 6
 
 
 def test_scene_camera():
     "Test a scene constructed with a camera object."
+
     camera = Camera(position=(-50.28, -30.32, 58.64), direction=(0.59, 0.44, -0.67),
                     up_vector=(0.53, 0.40, 0.74), h_size=52.90)
     scene = Scene(background_color=(255, 255, 255), cameras=camera.to_vtk())
@@ -114,6 +121,7 @@ def test_scene_camera():
 
 def test_scene_cameras():
     """Test a scene constructed with a list of camera objects."""
+
     file_path = r'./tests/assets/viewbased.hbjson'
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh,
                               load_views=True)
@@ -124,6 +132,7 @@ def test_scene_cameras():
 
 def test_add_cameras():
     """Test adding a list of cameras."""
+
     file_path = r'./tests/assets/viewbased.hbjson'
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
     # Check is valuerror is raised in case the radiance views are not loaded on model
@@ -148,6 +157,7 @@ def test_add_cameras():
 
 def test_add_camera():
     """Test adding a camera."""
+
     camera = Camera(position=(-50.28, -30.32, 58.64), direction=(0.59, 0.44, -0.67),
                     up_vector=(0.53, 0.40, 0.74), h_size=52.90)
     scene = Scene(background_color=(255, 255, 255))
@@ -156,11 +166,14 @@ def test_add_camera():
     assert len(scene.cameras) == 2
 
 
-def test_DELETE():
+def test_export_image():
+    """Test export images method."""
     file_path = r'./tests/assets/gridbased.hbjson'
     results_folder = r'./tests/assets/df_results'
+    target_folder = r'./tests/assets/temp'
 
-    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh,
+                              load_views=True)
 
     daylight_factor = []
     for grid in model.sensor_grids.data:
@@ -171,15 +184,90 @@ def test_DELETE():
     model.sensor_grids.add_data_fields(daylight_factor, name='Daylight-factor',
                                        per_face=True, data_range=(0, 20))
     model.sensor_grids.color_by = 'Daylight-factor'
-
     model.sensor_grids.display_mode = DisplayMode.SurfaceWithEdges
-    model.update_display_mode(DisplayMode.SurfaceWithEdges)
+    model.update_display_mode(DisplayMode.Wireframe)
 
+    # Setup legend
+    color_range = model.sensor_grids.active_field_info.color_range()
+
+    # Initialize a scene
+    scene = Scene(model=model, background_color=(255, 255, 255),
+                  monochrome=True, monochrome_color=(0.0, 0.0, 0.0))
+
+    # A camera setup using the constructor
     camera = Camera(position=(-50.28, -30.32, 58.64), direction=(0.59, 0.44, -0.67),
                     up_vector=(0.53, 0.40, 0.74), h_size=52.90)
 
-    scene = Scene(background_color=(255, 255, 255), cameras=camera.to_vtk())
-    scene.add_model(model)
+    # Add all the camera to the scene
+    scene.add_cameras(camera.to_vtk())
+
+    interactor, window = scene.create_render_window()[0:2]
+
+    # if target folder exists, delete it and create a fresh new folder
+    if os.path.isdir(target_folder):
+        shutil.rmtree(target_folder)
+    os.mkdir(target_folder)
+
+    # Export images for all the cameras
+    path = scene.export_image(
+        folder=target_folder, window=window, interactor=interactor,
+        image_type=ImageTypes.png, name='camera', color_range=color_range)
+
+    assert os.path.isfile(path)
+    shutil.rmtree(target_folder)
+
+
+def test_export_images():
+    """Test export images method."""
+    file_path = r'./tests/assets/gridbased.hbjson'
+    results_folder = r'./tests/assets/df_results'
+    target_folder = r'./tests/assets/temp'
+
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh,
+                              load_views=True)
+
+    daylight_factor = []
+    for grid in model.sensor_grids.data:
+        res_file = pathlib.Path(results_folder, f'{grid.identifier}.res')
+        grid_res = [float(v) for v in res_file.read_text().splitlines()]
+        daylight_factor.append(grid_res)
+
+    model.sensor_grids.add_data_fields(daylight_factor, name='Daylight-factor',
+                                       per_face=True, data_range=(0, 20))
+    model.sensor_grids.color_by = 'Daylight-factor'
+    model.sensor_grids.display_mode = DisplayMode.SurfaceWithEdges
+    model.update_display_mode(DisplayMode.Wireframe)
+
+    # Setup legend
     color_range = model.sensor_grids.active_field_info.color_range()
-    scene.to_image(folder=r'./tests/assets/', image_type=ImageTypes.png,
-                   name='1', color_range=color_range)
+
+    # Initialize a scene
+    scene = Scene(model=model, background_color=(255, 255, 255),
+                  monochrome=True, monochrome_color=(0.0, 0.0, 0.0))
+
+    # A camera setup using the constructor
+    camera = Camera(position=(-50.28, -30.32, 58.64), direction=(0.59, 0.44, -0.67),
+                    up_vector=(0.53, 0.40, 0.74), h_size=52.90)
+
+    # Cameras extracted from hbjson
+    cameras = Camera.from_model(model)
+
+    # Gather all the cameras
+    cameras.append(camera.to_vtk())
+
+    # Add all the cameras to the scene
+    scene.add_cameras(cameras)
+
+    # if target folder exists, delete it and create a fresh new folder
+    if os.path.isdir(target_folder):
+        shutil.rmtree(target_folder)
+    os.mkdir(target_folder)
+
+    # Export images for all the cameras
+    images_path = scene.export_images(folder=target_folder, image_type=ImageTypes.png,
+                                      name='camera', color_range=color_range)
+
+    for path in images_path:
+        assert os.path.isfile(path)
+    shutil.rmtree(target_folder)
+    
