cc147b8354b4974bfd4a15f97575bb379ab98227
Devang <devngc@gmail.com>
2021-05-13 21:03:00 +0530

    style(types): add missing spaces around operators

--- honeybee_vtk/types.py
+++ honeybee_vtk/types.py
@@ -376,7 +376,7 @@ class ModelDataSet:
 
     def rgb_to_decimal(self):
         """RGB color in decimal."""
-        return (self.color[0]/255, self.color[1]/255, self.color[2]/255)
+        return (self.color[0] / 255, self.color[1] / 255, self.color[2] / 255)
 
     def to_folder(self, folder, sub_folder=None) -> str:
         """Write data information to a folder.
