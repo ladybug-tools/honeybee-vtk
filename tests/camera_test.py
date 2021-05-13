334aa7ef6ca6ac517ade8f7860171819c2db1cb5
Devang <devngc@gmail.com>
2021-05-13 21:05:17 +0530

    tests(camera_test): add unit tests for camera

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -2,6 +2,7 @@
 
 import pytest
 import vtk
+from ladybug_geometry.geometry3d.pointvector import Point3D
 from honeybee_vtk.camera import Camera
 from honeybee_vtk.model import Model
 from honeybee_vtk.vtkjs.schema import SensorGridOptions
@@ -14,12 +15,20 @@ def test_to_vtk():
     # Initialize a camera object and assess all the default properties
     camera = Camera()
     assert camera.identifier[0:6] == 'camera'
-    assert camera.position == (0, 0, 50)
-    assert camera.direction == (0, 0, -1)
-    assert camera.up_vector == (0, 1, 0)
-    assert camera.h_size == 60
-    assert camera.v_size == 60
-    assert camera.view_type == 'v'
+    assert camera.position.value == (0, 0, 50)
+    assert camera.direction.value == (0, 0, -1)
+    assert camera.up_vector.value == (0, 1, 0)
+    assert camera.h_size.value == 60
+    assert camera.v_size.value == 60
+    assert camera.type.value == 'v'
+    assert not camera.bounds
+    assert camera.camera_offset == 1
+    assert camera.flat_view_direction[(0, 0, -1)] == [2, '+']
+    assert camera.flat_view_direction[(0, 0, 1)] == [2, '-']
+    assert camera.flat_view_direction[(0, 1, 0)] == [1, '+']
+    assert camera.flat_view_direction[(0, -1, 0)] == [1, '-']
+    assert camera.flat_view_direction[(-1, 0, 0)] == [0, '-']
+    assert camera.flat_view_direction[(1, 0, 0)] == [0, '+']
 
     # Assess type of the outcome of the to_vtk method
     camera = camera.to_vtk()
@@ -29,7 +38,8 @@ def test_to_vtk():
 def test_from_model():
     """Test if views are being read from hbjson."""
     file_path = r'./tests/assets/viewbased.hbjson'
-    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh,
+                              load_views=False)
 
     # Checking if valueerror is raised when from_model is called on a model with no views
     with pytest.raises(ValueError):
@@ -39,10 +49,47 @@ def test_from_model():
                               load_views=True)
 
     cameras = Camera.from_model(model)
-    cameras_check = [isinstance(camera, Camera) for camera in cameras]
-    assert cameras_check.count(True) == len(cameras)
+    check = [isinstance(camera, Camera) for camera in cameras]
+    assert check.count(True) == len(cameras)
 
 
+def test_assign_bounds():
+    """Test bounds assignment."""
+    file_path = r'./tests/assets/viewbased.hbjson'
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+    camera = Camera()
+
+    with pytest.raises(ValueError):
+        camera.bounds = model
+
+    actors = Actors(model)
+    bounds = actors.get_bounds()
+    camera.bounds = bounds
+    assert isinstance(camera.bounds, list)
+    check = [isinstance(point, Point3D) for point in bounds]
+    assert check.count(True) == len(bounds)
 
 
-    
+def test_assign_camera_offset():
+    """Test assigning camera offset."""
+    camera = Camera()
+
+    with pytest.raises(ValueError):
+        camera.camera_offset = 2.3
+
+    camera.camera_offset = 3
+    assert camera.camera_offset == 3
+
+
+def test_adjustable_postion():
+    """Test if correct adjustable position is being returned."""
+
+    bounds = [Point3D(-7.00, -4.00, 1.00), Point3D(12.00, 9.00, 5.00),
+              Point3D(-8.00, -4.00, 0.00), Point3D(13.00, 9.00, 5.00),
+              Point3D(-8.00, -7.00, 0.00), Point3D(12.00, 20.00, 15.00),
+              Point3D(-8.00, -4.00, 0.00), Point3D(13.00, 9.00, 0.00),
+              Point3D(-8.00, -4.00, 3.00), Point3D(13.00, 9.00, 5.00)]
+    camera = Camera(position=(0, 0, 5000), direction=(0, 0, -1), bounds=bounds)
+    assert camera._outermost_point() == Point3D(12.00, 20.00, 15.00)
+    assert camera._adjusted_position() == (0, 0, 16)
+
