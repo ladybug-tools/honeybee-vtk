f17676d6f2c0c46b0aa672afd1b56e90b34db975
Devang <devngc@gmail.com>
2021-05-21 15:28:39 +0530

    test(assistant_test): update test

--- tests/assistant_test.py
+++ tests/assistant_test.py
@@ -1,19 +1,25 @@
 """Unit test for assistants module."""
 
+from honeybee_vtk.scene import Scene
 import pytest
 import vtk
+from honeybee_vtk.model import Model
 from honeybee_vtk.assistant import Assistant
+from honeybee_vtk.actor import Actor
 from honeybee_vtk.camera import Camera
 
 
 def test_initialization():
     """Test objct initialization."""
 
-    with pytest.raises(TypeError):
-        assistant = Assistant()
-
+    file_path = r'./tests/assets/gridbased.hbjson'
+    model = Model.from_hbjson(hbjson=file_path)
+    actors = Actor.from_model(model=model)
     camera = Camera()
-    assistant = Assistant(background_color=(0, 0, 0), camera=camera)
+    scene = Scene()
+    scene.add_actors(actors)
+    scene.add_cameras(camera)
+    assistant = Assistant(background_color=(0, 0, 0), camera=camera, actors=scene._actors)
     assert isinstance(assistant._interactor, vtk.vtkRenderWindowInteractor)
     assert isinstance(assistant._window, vtk.vtkRenderWindow)
     assert isinstance(assistant._renderer, vtk.vtkRenderer)
