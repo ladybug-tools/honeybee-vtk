6539ac51815420d72c4842722362768580508d0b
Devang <devngc@gmail.com>
2021-05-26 21:44:20 +0530

    refactor(*): update modules for a multi legend image export

--- tests/scene_test.py
+++ tests/scene_test.py
@@ -11,6 +11,7 @@ from honeybee_vtk.scene import Scene
 from honeybee_vtk.vtkjs.schema import SensorGridOptions, DisplayMode
 from honeybee_vtk.camera import Camera
 from honeybee_vtk.actor import Actor
+from honeybee_vtk.types import ImageTypes
 
 
 def test_class_initialization():
@@ -153,3 +154,60 @@ def test_adding_data():
     assert 'Daylight-factor' in model.sensor_grids.fields_info.keys()
 
 
+def test_export_images():
+    """Test export images method."""
+    file_path = r'./tests/assets/gridbased.hbjson'
+    results_folder = r'./tests/assets/df_results'
+    target_folder = r'./tests/assets/temp'
+
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+
+    daylight_factor = []
+    for grid in model.sensor_grids.data:
+        res_file = pathlib.Path(results_folder, f'{grid.identifier}.res')
+        grid_res = [float(v) for v in res_file.read_text().splitlines()]
+        daylight_factor.append(grid_res)
+
+    model.sensor_grids.add_data_fields(daylight_factor, name='Daylight-factor',
+                                       per_face=True, data_range=(0, 20), add_legend=True)
+    model.sensor_grids.color_by = 'Daylight-factor'
+    model.sensor_grids.display_mode = DisplayMode.SurfaceWithEdges
+    model.update_display_mode(DisplayMode.Wireframe)
+
+    color_range = model.sensor_grids.active_field_info.get_lookuptable()
+    # actors
+    actors = Actor.from_model(model=model)
+
+    # Initialize a scene
+    scene = Scene(background_color=(255, 255, 255))
+    scene.add_actors(actors)
+
+    # A camera setup using the constructor
+    camera = Camera(position=(-50.28, -30.32, 58.64), direction=(0.59, 0.44, -0.67),
+                    up_vector=(0.53, 0.40, 0.74), h_size=52.90)
+
+    # Cameras extracted from hbjson
+    cameras = model.cameras
+
+    # Gather all the cameras
+    cameras.append(camera)
+
+    # Add all the cameras to the scene
+    scene.add_cameras(cameras)
+
+    # if target folder exists, delete it and create a fresh new folder
+    if os.path.isdir(target_folder):
+        shutil.rmtree(target_folder)
+    os.mkdir(target_folder)
+
+    # Export images for all the cameras
+    images_path = scene.export_images(folder=target_folder, image_type=ImageTypes.png,
+                                      name='camera', color_range=color_range)
+
+    for path in images_path:
+        assert os.path.isfile(path)
+
+    shutil.rmtree(target_folder)
+
+
+    
