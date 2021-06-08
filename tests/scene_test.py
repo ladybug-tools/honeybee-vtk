d25b461ce61a03364965d6b60f59a3838a3e585c
Devang <devngc@gmail.com>
2021-06-08 21:41:47 +0530

    test(*): update unit tests

--- tests/scene_test.py
+++ tests/scene_test.py
@@ -13,7 +13,7 @@ from honeybee_vtk.vtkjs.schema import SensorGridOptions, DisplayMode
 from honeybee_vtk.camera import Camera
 from honeybee_vtk.actor import Actor
 from honeybee_vtk.types import ImageTypes, Colors
-from honeybee_vtk.legend_parameter import Orientation
+from honeybee_vtk.legend_parameter import LabelFormat, Orientation
 
 
 def test_class_initialization():
