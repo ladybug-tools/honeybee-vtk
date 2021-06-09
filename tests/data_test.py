0bec99c8584fe953d9b68545e61cfa219c2780aa
Devang <devngc@gmail.com>
2021-06-09 19:52:52 +0530

    test(data_test): add unit tests

--- tests/data_test.py
+++ tests/data_test.py
@@ -1,8 +1,34 @@
 """Unit tests for data module."""
 
 import pytest
+from pydantic import ValidationError
 from honeybee_vtk.model import Model
+from honeybee_vtk.data import check_data_config
+from honeybee_vtk.vtkjs.schema import SensorGridOptions
 
 
-def test_mounting_grid_data():
+file_path = r'./tests/assets/gridbased.hbjson'
+model = Model.from_hbjson(hbjson=file_path, load_grids=SensorGridOptions.Mesh)
+
+
+def test_invalid_path():
+    """Test whether invalid path of json raises an error."""
+    invalid_json_path = r'./tests/assets/config/valids.json'
+
+    with pytest.raises(FileExistsError):
+        config = check_data_config(invalid_json_path, model)
+
+
+def test_invalid_json():
+    """Test whether error is raised for an invalid json."""
+    invalid_config = r'./tests/assets/config/invalid.json'
+
+    with pytest.raises(TypeError):
+        config = check_data_config(invalid_config, model)
+
+
+def test_valid_json():
     """Test if data for the grid can be assessed successfully."""
+    valid_config = r'./tests/assets/config/valid.json'
+    config = check_data_config(valid_config, model)
+    assert isinstance(config, dict)
