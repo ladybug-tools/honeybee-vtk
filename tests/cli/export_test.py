f9739c001dcfda84d6c0b25f5108aad651316451
Devang <devngc@gmail.com>
2021-06-09 19:53:18 +0530

    test(export_test): add config to unit test

--- tests/cli/export_test.py
+++ tests/cli/export_test.py
@@ -1,5 +1,7 @@
+import traceback
 import pytest
 import os
+from traceback import TracebackException
 from click.testing import CliRunner
 from honeybee_vtk.cli.export import export
 from ladybug.futil import nukedir
@@ -10,12 +12,17 @@ from ladybug.futil import nukedir
 #     runner = CliRunner()
 #     file_path = './tests/assets/gridbased.hbjson'
 #     target_folder = './tests/assets/target'
+#     view_file = './tests/assets/view.vf'
+#     view_file_1 = './tests/assets/view1.vf'
+#     config_path = './tests/assets/config/valid.json'
 
 #     result = runner.invoke(
-#         export, [file_path, '--folder', target_folder, '--name', 'Model', '--image-type',
-#                  'PNG', '--image-width', 200, '--image-height', 500,
-#                  '--background-color', 255, 255, 255, '--grid-options', 'Meshes',
-#                  '--display-mode-model', 'Shaded', '--display-mode-grid', 'SurfaceWithEdges'])
+#         export, [
+#             file_path, '--folder', target_folder, '--name', 'Model', '--image-type',
+#             'PNG', '--image-width', 200, '--image-height', 500,
+#             '--background-color', 255, 255, 255, '--grid-options', 'Meshes',
+#             '--display-mode-model', 'Shaded', '--display-mode-grid', 'SurfaceWithEdges',
+#             '--view', view_file, '--view', view_file_1, '--data-config', config_path])
 
 #     assert result.exit_code == 0
 #     images_path = os.listdir(target_folder)
