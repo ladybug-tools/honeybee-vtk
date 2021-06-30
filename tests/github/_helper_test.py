0bd65b3037bcdec85eff77d934db9481f6dcec48
Devang <devngc@gmail.com>
2021-06-30 14:38:06 +0530

    test(_helper_test): update test

--- tests/github/_helper_test.py
+++ tests/github/_helper_test.py
@@ -7,17 +7,28 @@ from honeybee_vtk._helper import _validate_input, get_min_max
 def test_check_tuple():
     """Test all the functionalities of this method."""
     val = (1, 2, 3)
-    assert _validate_input(val, int, max_val=4)
+    assert _validate_input(val, [int], max_val=4)
 
     val = (0.1, 0.2, 0.3)
-    assert _validate_input(val, float, 1)
-    assert _validate_input(val, float)
+    assert _validate_input(val, [float], 1)
+    assert _validate_input(val, [float])
 
     val = (1, 2, 3)
-    assert not _validate_input(val, float)
+    assert not _validate_input(val, [float])
 
     val = (1.2, 3.4, 5.4)
-    assert not _validate_input(val, float, 1.0)
+    assert not _validate_input(val, [float], 1.0)
+
+    val = (1.2, 3.4, 5.4)
+    with pytest.raises(AssertionError):
+        _validate_input(val, [float], 1.0, 2)
+
+    val = (1, 3, 5)
+    with pytest.raises(AssertionError):
+        _validate_input(val, [float], num_val=2)
+
+    val = (1.5, 3, 5)
+    assert _validate_input(val, val_type=[int, float])
 
 
 def test_get_min_max():
