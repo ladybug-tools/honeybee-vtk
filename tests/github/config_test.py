a64ab05deb23d53f8c5613017aa7785733183b73
Devang <devngc@gmail.com>
2021-07-01 21:25:40 +0530

    test(*): update tests

--- tests/github/config_test.py
+++ tests/github/config_test.py
@@ -5,6 +5,8 @@ import pytest
 from pydantic import ValidationError
 from honeybee_vtk.model import Model
 from honeybee_vtk.scene import Scene
+from honeybee_vtk.camera import Camera
+from honeybee_vtk.actor import Actor
 from honeybee_vtk.config import load_config
 from honeybee_vtk.vtkjs.schema import SensorGridOptions
 
@@ -16,11 +18,9 @@ def test_data_config():
     valid_json_path = r'tests/assets/config/valid.json'
     invalid_json_path = r'tests/assets/config/invalid.json'
     more_grids = r'tests/assets/config/more_grids.json'
-    no_file = r'tests/assets/config/no_file.json'
-    more_files = r'tests/assets/config/more_files.json'
     short_length = r'tests/assets/config/short_length.json'
     invalid_object_type = r'tests/assets/config/invalid_object_type.json'
-    invalid_file_paths = r'tests/assets/config/invalid_file_paths.json'
+    invalid_folder_path = r'tests/assets/config/invalid_folder_path.json'
 
     model = Model.from_hbjson(file_path)
     scene = Scene()
@@ -34,18 +34,15 @@ def test_data_config():
         load_config(valid_json_path, model, scene)
 
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+    scene = Scene()
+    cameras = model.cameras
+    actors = Actor.from_model(model)
+    scene.add_cameras(cameras)
+    scene.add_actors(actors)
 
     # trying to load more grids than grids in the model
     with pytest.raises(ValueError):
         load_config(more_grids, model, scene)
-
-    # if Empty list is provided in file_paths
-    with pytest.raises(ValueError):
-        load_config(no_file, model, scene)
-
-    # If more than one files are provided in file_path for non-grid object_type
-    with pytest.raises(ValueError):
-        load_config(more_files, model, scene)
 
     # if file lengths do not match grid size
     with pytest.raises(ValueError):
@@ -57,7 +54,7 @@ def test_data_config():
 
     # invalid file paths
     with pytest.raises(ValidationError):
-        load_config(invalid_file_paths, model, scene)
+        load_config(invalid_folder_path, model, scene)
 
 
 def test_legend_config():
@@ -66,25 +63,29 @@ def test_legend_config():
     file_path = r'tests/assets/gridbased.hbjson'
     invalid_colors = r'tests/assets/config/invalid_colors.json'
     invalid_orientation = r'tests/assets/config/invalid_orientation.json'
-    invalid_label_format = r'tests/assets/config/invalid_label_format.json'
-    color_by = r'tests/assets/config/color_by.json'
+    invalid_decimal_count = r'tests/assets/config/invalid_decimal_count.json'
+    hide_data = r'tests/assets/config/hide_data.json'
 
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
     scene = Scene()
+    cameras = model.cameras
+    actors = Actor.from_model(model)
+    scene.add_cameras(cameras)
+    scene.add_actors(actors)
 
     # if invalid colors in legend parameters raises error
-    with pytest.raises(ValidationError):
+    with pytest.raises(KeyError):
         load_config(invalid_colors, model, scene)
 
     # if invalid orientation in legend parameters raises error
     with pytest.raises(ValidationError):
         load_config(invalid_orientation, model, scene)
 
-    # if invalid Label_format in legend parameters raises error
-    with pytest.raises(ValidationError):
-        load_config(invalid_label_format, model, scene)
+    # if invalid decimal count in legend parameters raises error
+    with pytest.raises(KeyError):
+        load_config(invalid_decimal_count, model, scene)
 
     # if warning is raised in case legend parameters are provided for data that is
     # not going to color the object_type
     with pytest.warns(Warning):
-        load_config(color_by, model, scene)
+        load_config(hide_data, model, scene)
