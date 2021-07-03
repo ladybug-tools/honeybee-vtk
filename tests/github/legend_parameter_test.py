100c4ccd4e7583bdcc9245fd109b54f3f06b93ea
Devang <devngc@gmail.com>
2021-07-03 18:50:52 +0530

    test(legend_parameter_test): add and update unit tests

--- tests/github/legend_parameter_test.py
+++ tests/github/legend_parameter_test.py
@@ -39,7 +39,7 @@ def test_font_errors():
 def test_legend_parameter_initialization():
     """Testing initialization of a legend_parameter object."""
 
-    lp = LegendParameter(range=(0, 100))
+    lp = LegendParameter()
 
     assert lp.name == 'Legend'
     assert lp.colors == ColorSets.ecotect
@@ -66,7 +66,7 @@ def test_legend_parameter_initialization():
 def test_legend_parameter_errors():
     """Test if correct exceptions are raised."""
 
-    lp = LegendParameter(range=(0, 100))
+    lp = LegendParameter()
 
     with pytest.raises(ValueError):
         lp.name = 2
