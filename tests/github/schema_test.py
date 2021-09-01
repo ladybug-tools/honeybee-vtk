aa82cc9e525b763690328961294d8fef6308f615
Devang <devngc@gmail.com>
2021-09-01 19:35:01 +0530

    test(schema_test): start unit tests for schema

---
+++ tests/github/schema_test.py
@@ -0,0 +1,13 @@
+"""Unit test for the schema module."""
+
+import pytest
+from honeybee_vtk.vtkjs.schema import DataSet, DataSetResource
+
+# TODO: more unit tests to be added to cover the whole schema
+
+
+def test_default_legend_ranges():
+    """Check default values of legend_ranges."""
+    dr = DataSetResource(url='.')
+    ds = DataSet(name='temp', httpDataSetReader=dr)
+    assert ds.legend_ranges == {}
