9b6ebf0990b29c2d8212e3e636b3887ef4f46e39
Devang <devngc@gmail.com>
2022-02-15 14:28:26 +0530

    test(model_test): add and update unit tests

--- tests/local/model_test.py
+++ tests/local/model_test.py
@@ -19,40 +19,19 @@ def test_grid_to_images():
         shutil.rmtree(target_folder)
     os.mkdir(target_folder)
 
-    # convert all the grids to images
-    model.to_grid_images(json_path, folder=target_folder)
-    for count, file in enumerate(os.listdir(target_folder)):
-        assert file == f'Daylight-Factor_TestRoom_{count+1}.png'
+    file_names = ['Daylight-Factor_TestRoom_1.png', 'Daylight-Factor_TestRoom_2.png',
+                  'UDI_TestRoom_1.png', 'UDI_TestRoom_2.png', ]
+    model.to_grid_images(config=json_path, folder=target_folder)
+    for file in os.listdir(target_folder):
+        assert file in file_names
 
-    # remove the folder
     shutil.rmtree(target_folder)
 
 
-def test_grid_to_images_assertion_error():
-    """Test if grids are being converted to images."""
+def test_grid_to_images():
+    """Test if grids selected grids are being exported."""
     file_path = r'tests/assets/gridbased.hbjson'
     json_path = r'tests/assets/config/valid.json'
-    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Ignore)
-
-    # create a folder to store the images
-    target_folder = r'tests/assets/temp'
-    if os.path.isdir(target_folder):
-        shutil.rmtree(target_folder)
-    os.mkdir(target_folder)
-
-    # Make sure the assertion error is raised when this method is used on a model
-    # that does not have grids loaded.
-    with pytest.raises(AssertionError):
-        model.to_grid_images(json_path, folder=target_folder)
-
-    # remove the folder
-    shutil.rmtree(target_folder)
-
-
-def test_grid_to_images_value_error():
-    """Test if grids are being converted to images."""
-    file_path = r'tests/assets/gridbased.hbjson'
-    json_path = r'tests/assets/config/valid_with_invalid_grid_filter.json'
     model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
 
     # create a folder to store the images
@@ -61,10 +40,45 @@ def test_grid_to_images_value_error():
         shutil.rmtree(target_folder)
     os.mkdir(target_folder)
 
-    # Make sure the assertion error is raised when this method is used on a model
-    # that does not have grids loaded.
-    with pytest.raises(ValueError):
-        model.to_grid_images(json_path, folder=target_folder)
+    file_names = ['Daylight-Factor_TestRoom_1.png', 'UDI_TestRoom_1.png']
+    model.to_grid_images(config=json_path, folder=target_folder,
+                         grid_filter=['TestRoom_1'])
+    for file in os.listdir(target_folder):
+        assert file in file_names
 
-    # remove the folder
+    shutil.rmtree(target_folder)
+
+
+def test_grid_to_images_assertion_error():
+    """Test if AssertionError is raised when grids are not loaded."""
+    file_path = r'tests/assets/gridbased.hbjson'
+    json_path = r'tests/assets/config/valid.json'
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Ignore)
+
+    target_folder = r'tests/assets/temp'
+    if os.path.isdir(target_folder):
+        shutil.rmtree(target_folder)
+    os.mkdir(target_folder)
+
+    with pytest.raises(AssertionError):
+        model.to_grid_images(config=json_path, folder=target_folder)
+
+    shutil.rmtree(target_folder)
+
+
+def test_grid_to_images_value_error():
+    """Test if valueerror is raised on wrong grid identifier."""
+    file_path = r'tests/assets/gridbased.hbjson'
+    json_path = r'tests/assets/config/valid.json'
+    model = Model.from_hbjson(file_path, load_grids=SensorGridOptions.Mesh)
+
+    target_folder = r'tests/assets/temp'
+    if os.path.isdir(target_folder):
+        shutil.rmtree(target_folder)
+    os.mkdir(target_folder)
+
+    with pytest.raises(ValueError):
+        model.to_grid_images(config=json_path, folder=target_folder,
+                             grid_filter=['abc'],)
+
     shutil.rmtree(target_folder)
