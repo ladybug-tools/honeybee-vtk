82849e9e9802cd2ab90a596f002dcacf81ed6f29
Devang <devngc@gmail.com>
2022-03-07 13:57:25 -0500

    refactor(scene): update legend_parameters property

--- honeybee_vtk/scene.py
+++ honeybee_vtk/scene.py
@@ -42,6 +42,8 @@ class Scene:
         self.background_color = background_color
         self.actors = actors or []
         self.cameras = cameras or []
+        self.legend_parameters = {actor.legend_parameter.name: actor.legend_parameter
+                                  for actor in self._actors}
         self.text_actor = text_actor
         self._assistants = []
 
@@ -122,10 +124,12 @@ class Scene:
     @property
     def legend_parameters(self) -> Dict[str, LegendParameter]:
         """Legends in the scene that can be added to the images."""
-        legends_dict = {}
-        for actor in self._actors:
-            legends_dict[actor.legend_parameter.name] = actor.legend_parameter
-        return legends_dict
+        return self._legend_parameters
+
+    @legend_parameters.setter
+    def legend_parameters(self, val: Dict[str, LegendParameter]) -> None:
+        """Set legend parameters for the scene."""
+        self._legend_parameters = val
 
     def legend_parameter(self, name: str) -> LegendParameter:
         """Get a legend parameter object by name.
