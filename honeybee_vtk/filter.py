e1bcdb4bdd9730df8baeda18a71703e6125127d3
Devang <devngc@gmail.com>
2022-03-07 13:31:39 -0500

    feat(filter): add the thresholds filter

---
+++ honeybee_vtk/filter.py
@@ -0,0 +1,41 @@
+"""Filters to be used with VTK."""
+
+import vtk
+
+
+def filter_using_thresholds(polydata: vtk.vtkPolyData, lower_threshold: float = None,
+                            upper_threshold: float = None) -> vtk.vtkPolyData:
+    """Apply a threshold filter to a polydata.
+
+    This function will remove the cell or points of the Polydata that fall outside of
+    the given thresholds. This function first applie the vtkThreshold filter to the
+    polydata and then uses the vtkDataSetSurfaceFilter to turn the 
+    vtkUnstructuredGrid into a vtkPolyData.
+
+    Args:
+        polydata: A vtkPolyData object.
+        lower_threshold: The lower threshold value. If None, lower threshold will 
+            be inifinite.
+        upper_threshold: The upper threshold value. If None, upper threshold will
+            be inifinite.
+
+    Returns:
+        A vtkPolyData object.
+    """
+    if lower_threshold is None and upper_threshold is None:
+        return polydata
+
+    threshold = vtk.vtkThreshold()
+    threshold.SetInputData(polydata)
+    if lower_threshold is not None:
+        threshold.SetLowerThreshold(lower_threshold)
+    if upper_threshold is not None:
+        threshold.SetUpperThreshold(upper_threshold)
+    threshold.AllScalarsOn()
+    threshold.Update()
+
+    surfacefilter = vtk.vtkDataSetSurfaceFilter()
+    surfacefilter.SetInputData(threshold.GetOutput())
+    surfacefilter.Update()
+
+    return surfacefilter.GetOutput()
