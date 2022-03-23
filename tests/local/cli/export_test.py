0b9636ca786e2fb209d1b420f5cdffbc49f8876c
Devang <devngc@gmail.com>
2022-03-23 14:21:45 -0400

    test(export_test): add unit tests for gif and transparent images export

--- tests/local/cli/export_test.py
+++ tests/local/cli/export_test.py
@@ -1,5 +1,6 @@
 
 import os
+from pathlib import Path
 from click.testing import CliRunner
 from honeybee_vtk.cli.export import export
 from ladybug.futil import nukedir
@@ -56,5 +57,66 @@ def test_export_grid_images():
     exported_file_names = os.listdir(f'{target_folder}/TestRoom_1')
     file_names = ['Daylight-Factor_TestRoom_1.png', 'UDI_TestRoom_1.png']
     assert all([name in file_names for name in exported_file_names])
+
+    nukedir(target_folder, True)
+
+
+def test_export_timestep_gif():
+    """"Test exporting a gif for the timesteps."""
+    runner = CliRunner()
+    target_folder = r'tests/assets/target'
+    file_path = r'tests/assets/gridbased_with_timesteps/inputs/model/gridbased.hbjson'
+    config_path = r'tests/assets/gridbased_with_timesteps/config.json'
+    peridos_path = r'tests/assets/gridbased_with_timesteps/periods.json'
+
+    nukedir(target_folder, True)
+
+    result = runner.invoke(
+        export, [
+            file_path, '--folder', target_folder, '-go', 'meshes', '--config',
+            config_path, '-sel', 'timesteps', '-tfn', 'sun-up-hours', '-pf',
+            peridos_path])
+
+    assert result.exit_code == 0
+
+    parent_folder_names = ('TestRoom_1_gif', 'TestRoom_2_gif')
+
+    target_folder = Path(target_folder)
+    for item in target_folder.rglob('*.gif'):
+        assert item.parent.stem in parent_folder_names and item.stem == 'output'
+
+    nukedir(target_folder, True)
+
+
+def test_export_timestep_gif_and_images():
+    """"Test exporting a gif and transparent images for the timesteps."""
+    runner = CliRunner()
+    target_folder = r'tests/assets/target'
+    file_path = r'tests/assets/gridbased_with_timesteps/inputs/model/gridbased.hbjson'
+    config_path = r'tests/assets/gridbased_with_timesteps/config.json'
+    peridos_path = r'tests/assets/gridbased_with_timesteps/periods.json'
+
+    nukedir(target_folder, True)
+
+    result = runner.invoke(
+        export, [
+            file_path, '--folder', target_folder, '-go', 'meshes', '--config',
+            config_path, '-sel', 'timesteps', '-tfn', 'sun-up-hours', '-pf',
+            peridos_path, '--transparent-images'])
+
+    assert result.exit_code == 0
+
+    gif_parent_folder_names = ('TestRoom_1_gif', 'TestRoom_2_gif')
+
+    target_folder = Path(target_folder)
+    for item in target_folder.rglob('*.gif'):
+        assert item.parent.stem in gif_parent_folder_names and item.stem == 'output'
+
+    images_parent_folder_names = ('TestRoom_1_images', 'TestRoom_2_images')
+    image_names = ('0', '1', '2')
+
+    for item in target_folder.rglob('*.png'):
+        assert item.parent.stem in images_parent_folder_names and \
+            item.stem in image_names
 
     nukedir(target_folder, True)
