c270306bac348c14c51b386e3c9b8ec807f70861
Devang <devngc@gmail.com>
2022-03-28 18:38:36 -0400

    test(post_process_test): add unit test for transparent-images command

--- tests/github/cli/post_process_test.py
+++ tests/github/cli/post_process_test.py
@@ -27,7 +27,32 @@ def test_gif():
 
     gif_names = ('TestRoom_1', 'TestRoom_2')
     for file_path in target_folder.rglob('*.gif'):
-        print(file_path)
         assert file_path.stem in gif_names
+
+    nukedir(target_folder, True)
+
+
+def test_transparent_images():
+    """Test cli command."""
+    runner = CliRunner()
+    images_folder = r'tests/assets/time_step_images'
+    target_folder = r'tests/target'
+
+    target_folder = Path(target_folder)
+    if not target_folder.exists():
+        target_folder.mkdir()
+
+    result = runner.invoke(
+        postprocess, [
+            'transparent-images', images_folder, '--folder', target_folder,
+            '--transparency', 0.6])
+
+    assert result.exit_code == 0
+
+    parent_folder_names = ('TestRoom_1_trans_images', 'TestRoom_2_trans_images')
+    file_names = ('0', '1', '2')
+    for file_path in target_folder.rglob('*.png'):
+        assert file_path.parent.stem in parent_folder_names and\
+            file_path.stem in file_names
 
     nukedir(target_folder, True)
